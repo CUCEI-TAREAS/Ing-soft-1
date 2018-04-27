40a9ca4a589b90733e504df620614771b9305693
Felipe de Jesus Ruiz Garcia <felipe_57@live.com.mx>
2018-04-27 18:00:19 -0500

    implementing /example resource to deploy template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
+from .models import User
 # Create your views here.
+
+class IndexView(generic.ListView):
+    # context_object_name = 'latest_question_list'
+    model = User
+    template_name = 'core/cv.htm'
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return('-e')
+
